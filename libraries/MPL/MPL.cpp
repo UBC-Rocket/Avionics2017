e8deb22a5b5d4c798b88f8dabc475d1afa76a360
YYen Gallup <yyengallup@gmail.com>
2017-01-15 12:04:45 -0800

    clean up of variable names, defines, formatting etc.

--- libraries/MPL/MPL.cpp
+++ libraries/MPL/MPL.cpp
@@ -3,11 +3,21 @@
 
 //#define PWR_MNG 0x6B //register address of pwr management
 //#define USR_CNTRL 0x6A //register address of user control
+#define WHO_AM_I 0x0C
+#define CTRL_REG1 0x26
+#define OUT_P_MSB 0x01
+#define OUT_T_MSB 0x04
+
+
+
+
+
 
 MPL::MPL(bool pickWire1) {
   wire = pickWire1;
   ground_set = false;
-  if(wire){
+
+  if(wire) {
      Wire1.begin();
   }
   else {
@@ -18,45 +28,46 @@ MPL::MPL(bool pickWire1) {
 
 //  Resets the altimeter via software
 void MPL::resetChip() {
-  writeByte(0x26, 0x04);
+  writeByte(CTRL_REG1, 0x04);
   init();
 }
 
 //  s to see that the altimeter is connected and functional
 //  and sets up the control registers
-boolean MPL::init() { 
-  readByte(0x0C) == 196;
-    
-  // CTRL_REG1 (0x26): enable sensor, oversampling x128, altimeter mode 
+boolean MPL::init() {
+  readByte(WHO_AM_I) == 196;
+
+  // CTRL_REG1 (0x26): enable sensor, oversampling x128, altimeter mode
   // writeByte(0x26, 0xBB);
-  writeByte(0x26, 0x81);
+  writeByte(CTRL_REG1, 0x81);
   // CTRL_REG4 (0x29): Data ready interrupt enbabled
   //writeByte(0x29, 0x00);
-    
+
   // Use these to set custom sea level pressures (Pressure in pascals at sea level / 2)
   //write(0x14, 0xC6);// BAR_IN_MSB (0x14):
   //write(0x15, 0x5B);// BAR_IN_LSB (0x15):
-	
-  //setGround(); //why cant you call it from here :( 
-  
-  num_samples_avg = NUM_SAMPLES_AVG;
-  
-  delay(1000); 
-	
+
+  //setGround(); //why cant you call it from here :(
+
+  num_samples_avg = 4;
+
+  delay(1000);
+
   return true;
 
 }
+
 void MPL::setGround() {
   ground_level = 0.0;
-  int readings = 100; //might want to think about the optimal number for this 
-  float finding_gnd = 0; 
+  int readings = 100; //might want to think about the optimal number for this
+  float finding_gnd = 0;
 
   for(int x=0; x < readings; x++){
-	delay (50); 
+	delay (50);
 	ground_level += readAltitude();
     //delay(100);
   }
-  ground_level /= readings; 
+  ground_level /= readings;
   ground_set = true;
 }
 
@@ -66,122 +77,119 @@ float MPL::getOffset() {
 
 //  Reads the current altitude in meters
 float MPL::readAltitude() {
-  writeByte(0x26, 0x83); //This forces the chip to make a reading 
-  writeByte(0x26, 0x81); //OST bit set to 1
+  writeByte(CTRL_REG1, 0x83); //This forces the chip to make a reading
+  writeByte(CTRL_REG1, 0x81); //OST bit set to 1
   int u_altitude, m_altitude;
   float l_altitude;
-  
-  uint8_t _buffer[3] = {0};
-  
-  readBytes(0x01, 3, _buffer);
-  
-  u_altitude = _buffer[0] << 8;//  The upper 8 bits of the altitude
-  m_altitude = _buffer[1];//  The middle 8 bits of the altitude
-  l_altitude = float(_buffer[2] >> 4) / 16.0;//  The lower 4 bits of the altitude
-  
-  
-  int16_t _temp = u_altitude | m_altitude;
+
+  uint8_t buffer[3] = {0};
+
+  readBytes(OUT_P_MSB, 3, buffer);
+
+  u_altitude = buffer[0] << 8;//  The upper 8 bits of the altitude
+  m_altitude = buffer[1];//  The middle 8 bits of the altitude
+  l_altitude = float(buffer[2] >> 4) / 16.0;//  The lower 4 bits of the altitude
+
+
+  int16_t temp = u_altitude | m_altitude;
   if(ground_set){
-    if (_temp < 0) {
-      return float(_temp) - l_altitude - ground_level;
+    if (temp < 0) {
+      return float(temp) - l_altitude - ground_level;
     }
     else {
-      return float(_temp) + l_altitude - ground_level;
+      return float(temp) + l_altitude - ground_level;
     }
   }
-  
+
   else {
-    if (_temp < 0) {
-      return float(_temp) - l_altitude;
+    if (temp < 0) {
+      return float(temp) - l_altitude;
     }
     else {
-      return float(_temp) + l_altitude;
+      return float(temp) + l_altitude;
     }
   }
-  
+
 }
 
 /*
  * Average your choice of altitude readings
- */ 
+ */
 float MPL::readAvgAltitude() {
-  
+
   int n;
   float avgAltitude = 0;
-  
+
   for (n = 0; n < num_samples_avg; n++){
 	  avgAltitude += readAltitude();
   }
-  
+
   avgAltitude /= num_samples_avg;
-  
+
   return avgAltitude;
-  
+
 }
 
-float MPL::readTemp()
-{
-  int8_t u_temp;
-  float l_temp;
-  uint8_t _buffer[2];
-  readBytes(0x04, 2, _buffer);
+float MPL::readTemp() {
+  int8_t utemp;
+  float ltemp;
+  uint8_t buffer[2];
+  readBytes(OUT_T_MSB, 2, buffer);
 
-  u_temp = _buffer[0];//  Upper 8 bits of the temperature, representing the numbers before the decimal
-  l_temp = float(_buffer[1] >> 4) / 16.0;//  Lower 4 bits of the temperature, representing the numbers 
-  
-  return float(u_temp) + l_temp;
+  utemp = buffer[0];//  Upper 8 bits of the temperature, representing the numbers before the decimal
+  ltemp = float(buffer[1] >> 4) / 16.0;//  Lower 4 bits of the temperature, representing the numbers
+
+  return float(utemp) + ltemp;
 }
 
 //  Writes a byte of data to the sensor at the given address WORKS
-byte MPL::writeByte(byte _regAddr, byte _value)
-{
-  if(!wire) {
-    Wire.beginTransmission(ADDRESS);
-    Wire.write(_regAddr);
-    Wire.write(_value);
-    return Wire.endTransmission(true);
-  } 
-  else {
+uint8_t MPL::writeByte(uint8_t regAddr, uint8_t value) {
+  if(wire) {
     Wire1.beginTransmission(ADDRESS);
-    Wire1.write(_regAddr);
-    Wire1.write(_value);
+    Wire1.write(regAddr);
+    Wire1.write(value);
     return Wire1.endTransmission(true);
+  }
+  else {
+    Wire.beginTransmission(ADDRESS);
+    Wire.write(regAddr);
+    Wire.write(value);
+    return Wire.endTransmission(true);
   }
 }
 
-int MPL::readBytes(byte _regAddr, uint8_t _length, uint8_t *_data){
-  if(!_data) return -1;
+int MPL::readBytes(uint8_t regAddr, uint8_t length, uint8_t *data) {
+  if(!data) return -1;
 
   if(wire) {
-  
+
     Wire1.beginTransmission(ADDRESS);
-    Wire1.write(_regAddr);
+    Wire1.write(regAddr);
     Wire1.endTransmission(false);// or I2C_NOSTOP?
-    Wire1.requestFrom(ADDRESS, _length); // Request the data...
-  
-    for (int _i = 0; _i < _length; _i++) {
-      _data[_i] = Wire1.read();
+    Wire1.requestFrom(ADDRESS, length); // Request the data...
+
+    for (int i = 0; i < length; i++) {
+      data[i] = Wire1.read();
     }
     return Wire1.endTransmission(true);
-  } 
-  
+  }
+
   else {
     Wire.beginTransmission(ADDRESS);
-    Wire.write(_regAddr);
+    Wire.write(regAddr);
     Wire.endTransmission(false);// or I2C_NOSTOP?
-    Wire.requestFrom(ADDRESS, _length); // Request the data...
-  
-    for (int _i = 0; _i < _length; _i++) {
-      _data[_i] = Wire.read();
+    Wire.requestFrom(ADDRESS, length); // Request the data...
+
+    for (int i = 0; i < length; i++) {
+      data[i] = Wire.read();
     }
 
     return Wire.endTransmission(true);
-    }
   }
+}
 
-  uint8_t MPL::readByte(uint8_t reg) {
+uint8_t MPL::readByte(uint8_t reg) {
     uint8_t data;
     readBytes(reg, 1, &data);
     return data;
-  }
-
+}
