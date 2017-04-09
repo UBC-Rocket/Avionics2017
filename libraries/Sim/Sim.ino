63894bf317b86926d8f3701d8d3cba87dc47869a
YYen Gallup <yyengallup@gmail.com>
2017-04-09 14:32:31 -0700

    Trim test data to prevent too much data

--- libraries/Sim/Sim.ino
+++ libraries/Sim/Sim.ino
@@ -5,11 +5,31 @@ MPU *mpu;
 MPL *mpl;
 
 void setup() {
-  mpu = new MPU();
+  Serial.begin(9600);
+  while(!Serial) {
+    delay(1);
+  }
+
+  Serial.println("Beginning...");
+
+
   mpl = new MPL();
+  Serial.println("MPL created");
+  mpu = new MPU();
+  Serial.println("Objects created");
 
   mpu->begin(0,0);
   mpl->begin(0,0);
+
+  Serial.println("objects initialized");
+
+  mpu->initAccel(16);
+
+  delay(2000);
+
+  float tmp[3];
+  Serial.println(mpu->readAccel(tmp));
+  Serial.println(tmp[2]);
 }
 
 void loop() {
